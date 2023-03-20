e9d06bb79d71b52764334f9ff70adb8b18b791c4
yichien <kevinyichienlee@gmail.com>
2023-03-20 23:34:39 +0800

    destruct state in useSelector to get specific info

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,8 +4,8 @@ import { removeCar } from "../store";
 
 const CardList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data; 
+  const cars = useSelector(({cars: { data, searchTerm }}) => {
+    return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
   });
 
   const handleCarDelete = (car) => {
